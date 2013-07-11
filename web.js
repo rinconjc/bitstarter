3b5828e52a965820d4b8aad3c0e49f0802285f42
Julio <jcrinconc@gmail.com>
2013-07-11 22:00:53 +1000

    read index.xml from web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var file = require("fs");
+    response.send(file.readFileSync('index.html'));
 });
 
 var port = process.env.PORT || 5000;
