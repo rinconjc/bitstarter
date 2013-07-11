e703837db9a0aebfe71b447940722ef7a1b766b4
Julio <jcrinconc@gmail.com>
2013-07-11 22:14:21 +1000

    fixed encoding?

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var file = require("fs");
-    response.send(file.readFileSync('index.html', {encoding:'utf-8'}));
+    response.send(file.readFileSync('index.html', {encoding:'utf8'}));
 });
 
 var port = process.env.PORT || 5000;
